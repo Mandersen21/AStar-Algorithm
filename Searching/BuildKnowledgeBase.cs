ebf84705a80f1a229d2f739718cf96f9f11cdee4
mandersen21 <mikkelandersen21@gmail.com>
2017-05-06 00:42:29 +0200

    commit.

---
+++ Searching/BuildKnowledgeBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    public class BuildKnowledgeBase
+    {
+        public List<Clause> MapKnowledgeBase()
+        {
+            Clause A = new Clause("A", new List<String> { "a", "v", "~b" });
+            Clause B = new Clause("B", new List<String> { "b" });
+            Clause C = new Clause("C", new List<String> { "c" });
+
+            return new List<Clause> {
+                A,
+                B,
+                C,
+            };
+        }
+
+    }
+}
